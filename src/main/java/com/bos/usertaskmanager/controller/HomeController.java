45b0088766e0d5e237efea9ee0c37568eca3d051
dohyunkim <dohyun.kim@bos-semi.com>
2025-03-14 13:21:37 +0900

    feat: Add sidebar contents

--- src/main/java/com/bos/usertaskmanager/controller/HomeController.java
+++ src/main/java/com/bos/usertaskmanager/controller/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("message", "Welcome to User Task Manager");
-        return "home";
+        return "index";
     }
 }
